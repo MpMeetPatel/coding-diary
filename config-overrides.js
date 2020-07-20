231b1ea7731b61eeeb4e65dac929bc37a2449182
mpmeetpatel <meetpatel.professional@gmail.com>
2020-07-20 17:29:12 +0530

    🌵🧨 refactor(purgecss): back to normal, need to fix purgecss on live

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,4 @@
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-const tailwindcss = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = function override(config, env) {
     config.plugins = config.plugins.map((plugin) => {
@@ -11,18 +9,6 @@ module.exports = function override(config, env) {
             });
         }
         return plugin;
-    });
-    require('react-app-rewire-postcss')(config, {
-        plugins: [
-            tailwindcss('./tailwind.config.js'),
-            require('autoprefixer'),
-            process.env.NODE_ENV === 'production' &&
-                purgecss({
-                    content: ['./public/index.html', './src/**/*.js'],
-                    defaultExtractor: (content) =>
-                        content.match(/[\w-/:]+(?<!:)/g) || [],
-                }),
-        ],
     });
     return config;
 };
